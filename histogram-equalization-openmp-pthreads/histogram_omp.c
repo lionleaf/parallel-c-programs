392968d3ca2cfcc59866a8c175b05359d2b9352a
Andreas Løve Selvik <andreas.l.selvik@gmail.com>
2014-10-07 00:42:02 +0200

    First implementation of histogram with openMP

--- histogram-equalization-openmp-pthreads/histogram_omp.c
+++ histogram-equalization-openmp-pthreads/histogram_omp.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <omp.h>
 #include "bmp.h"
 
 const int image_width = 512;
@@ -21,12 +22,17 @@ int main(int argc, char** argv){
 
 
     int* histogram = (int*)calloc(sizeof(int), color_depth);
+#pragma omp parallel for num_threads(n_threads)
     for(int i = 0; i < image_size; i++){
-        histogram[image[i]]++;
+        int image_val = image[i]; 
+#pragma omp critical
+        histogram[image_val]++;
     }
 
 
     float* transfer_function = (float*)calloc(sizeof(float), color_depth);
+    
+#pragma omp parallel for num_threads(n_threads) schedule(static,1)
     for(int i = 0; i < color_depth; i++){
         for(int j = 0; j < i+1; j++){
             transfer_function[i] += color_depth*((float)histogram[j])/(image_size);
@@ -34,6 +40,7 @@ int main(int argc, char** argv){
     }
 
 
+#pragma omp parallel for num_threads(n_threads)
     for(int i = 0; i < image_size; i++){
         output_image[i] = transfer_function[image[i]];
     }
