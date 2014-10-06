fc3daa0b37d4fa98b06362e75e3b966f17aea94c
Andreas Løve Selvik <andreas.l.selvik@gmail.com>
2014-10-07 01:39:52 +0200

    PThread implementation compiles and runs, but is not correct yet

--- histogram-equalization-openmp-pthreads/histogram_pthreads.c
+++ histogram-equalization-openmp-pthreads/histogram_pthreads.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
 #include "bmp.h"
 
 const int image_width = 512;
@@ -8,23 +9,22 @@ const int image_height = 512;
 const int image_size = 512*512;
 const int color_depth = 255;
 
-int main(int argc, char** argv){
+unsigned char* image;
+unsigned char* output_image;
+int n_threads;
 
-    if(argc != 3){
-        printf("Useage: %s image n_threads\n", argv[0]);
-        exit(-1);
-    }
-    int n_threads = atoi(argv[2]);
 
-    unsigned char* image = read_bmp(argv[1]);
-    unsigned char* output_image = malloc(sizeof(unsigned char) * image_size);
+void* run_thread(void* thread_id){
+    long t_id = (long) thread_id;
 
+    printf("Running on thread %ld of %d \n", (long) thread_id, n_threads);
+    
+    int chunk_size = image_size / n_threads ;
 
     int* histogram = (int*)calloc(sizeof(int), color_depth);
-    for(int i = 0; i < image_size; i++){
+    for(int i = chunk_size * t_id; i < chunk_size * (t_id + 1); i++){
         histogram[image[i]]++;
     }
-
 
     float* transfer_function = (float*)calloc(sizeof(float), color_depth);
     for(int i = 0; i < color_depth; i++){
@@ -38,5 +38,28 @@ int main(int argc, char** argv){
         output_image[i] = transfer_function[image[i]];
     }
 
+    pthread_exit(NULL);
+
+}
+
+int main(int argc, char** argv){
+
+
+    if(argc != 3){
+        printf("Useage: %s image n_threads\n", argv[0]);
+        exit(-1);
+    }
+    n_threads = atoi(argv[2]);
+
+    image = read_bmp(argv[1]);
+    output_image = malloc(sizeof(unsigned char) * image_size);
+    
+    pthread_t threads[n_threads];
+
+    for(long i = 0; i < n_threads; i++){
+        pthread_create(&threads[i], NULL, run_thread, (void*) i);
+    }
+
     write_bmp(output_image, image_width, image_height);
+    pthread_exit(NULL);
 }
